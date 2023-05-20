bd0bb0d471c2f08e536853cf96d166fce1f4157a
Anastasiia-Dubyna <122117575+Anastasiia-Dubyna@users.noreply.github.com>
2023-05-20 19:19:08 +0300

    add

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryListEl = document.querySelector(".gallery");
+
+function createGalleryListMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`
+    )
+    .join("");
+}
